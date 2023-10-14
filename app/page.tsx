97f9758c2c48ff832676ec3eea214fef2f712b2b
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-10-14 15:54:49 +0200

    Remove boilerplate from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold text-center">
-        Welcome to{' '}
-        <a className="text-blue-600" href="https://nextjs.org">
-          Next.js!
-        </a>
+        Norges Bondebridgeforbund
       </h1>
     </main>
   )
