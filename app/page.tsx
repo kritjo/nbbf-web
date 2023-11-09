a661deb0b629262af5099e354d111a0b915f2bdb
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-11-09 23:37:11 +0100

    Localization

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             <input
               className="w-full px-3 py-2 border border-gray-300 rounded-md"
               id="email"
-              placeholder="Enter your email"
+              placeholder="Din e-post"
               required
               type="email"
             />
