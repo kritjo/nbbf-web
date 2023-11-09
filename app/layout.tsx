7ecd1318e489c8eeeb7e216aba80e41467a641a1
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-11-09 22:56:14 +0100

    Layout using new components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import {Analytics} from '@vercel/analytics/react';
+import {ReactNode} from "react";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: 'Norges Bondebridgeforbund',
@@ -11,16 +14,20 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                     children,
+                                   }: {
+  children: ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-      </body>
+    <body className={inter.className}>
+    <section className="min-h-screen bg-gray-900 text-white">
+      <Navbar/>
+      {children}
+      <Footer/>
+    </section>
+    <Analytics/>
+    </body>
     </html>
   )
 }
