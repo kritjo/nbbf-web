4030b033a2625a8b69a69a676bec89437f1b5d54
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-11-09 22:55:46 +0100

    Footer using official data

---
+++ app/components/Footer.tsx
@@ -0,0 +1,16 @@
+import '@fontsource/pt-mono';
+
+const Footer = () => {
+  return (
+    <footer className="py-12 px-8 flex flex-col items-center space-y-2">
+      <img className="w-48 h-48" src="/nbbf-nt-nbg.png" alt="Norges Bondebridgeforbund Logo"/>
+      <p className="text-sm">Norges Bondebridgeforbund</p>
+      <p className="text-sm">c/o Kristian Tjelta Johansen</p>
+      <p className="text-sm">Sinsenveien 5A</p>
+      <p className="text-sm">0572 Oslo, Norge</p>
+      <p className="text-sm">NO 932 236 753 (Foretaksregisteret)</p>
+    </footer>
+  )
+}
+
+export default Footer
