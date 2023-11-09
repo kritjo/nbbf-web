bbed7140c410906b2e01a42a121bdc01b044ee41
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-11-09 23:35:58 +0100

    Contact page with only email

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,11 @@
+export default function Kontakt() {
+  return (
+    <main>
+      <section className="flex flex-col items-center justify-center py-12">
+        <a href="mailto:kristian@bondebridgeforbundet.no" className="text-2xl font-bold">
+          kristian@bondebridgeforbundet.no
+        </a>
+      </section>
+    </main>
+  )
+}
