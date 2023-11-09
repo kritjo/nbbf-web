9da081705c3a22d42a0961e660f75e7c2783823c
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-11-09 23:16:57 +0100

    Logo in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,21 +4,7 @@ const Navbar = () => {
   return (
     <header className="flex justify-between items-center px-8 py-4">
       <Link href="#">
-        <svg
-          className=" w-12 h-12"
-          fill="none"
-          height="24"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          width="24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"/>
-          <line x1="4" x2="4" y1="22" y2="15"/>
-        </svg>
+        <img className="w-24 h-24" src="/nbbf-nt-nbg.png" alt="Norges Bondebridgeforbund Logo"/>
       </Link>
       <nav className="space-x-4">
         <Link className="text-lg font-semibold hover:underline" href="#">
