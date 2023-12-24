ecb1e8fe19d289099b192bc929be1a6829250a50
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-24 14:18:18 +0100

    Send magic link serveraction

---
+++ app/actions.ts
@@ -0,0 +1,85 @@
+'use server'
+
+import {z} from "zod";
+import {db} from "../db/connection";
+import {magicLinks, users} from "../db/schema";
+import {eq} from "drizzle-orm";
+import {Resend} from "resend";
+
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+export type FormResponse = {
+  success: boolean,
+  errors: {
+    [key: string]: string[]
+  }
+}
+
+const schema = z.object({
+  email: z.string({
+    invalid_type_error: 'Invalid Email',
+  }),
+})
+
+export const sendMagicLink = async (_: any, formData: FormData): Promise<FormResponse> => {
+  const validatedFields = schema.safeParse({
+    email: formData.get('email'),
+  });
+
+  // Return early if the form data is invalid
+  if (!validatedFields.success) {
+    return {
+      success: false,
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+
+  console.log(`Magic link for ${validatedFields.data.email} requested`);
+
+  const user = await db.query.users.findFirst({
+    where: eq(users.email, validatedFields.data.email),
+  });
+
+  // Before continuing, wait random time between 0 and 0.5 seconds to prevent timing attacks
+  await sleep(Math.random() * 500);
+
+  if (!user) {
+    return {
+      success: true,
+      errors: {}
+    }
+  }
+
+  const randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const expiresAt = new Date();
+  expiresAt.setHours(expiresAt.getHours() + 1);
+  const {email} = validatedFields.data;
+  db.insert(magicLinks).values({
+    user: user.id,
+    slug: randomString,
+    used: false,
+    created_at: new Date(),
+    expires_at: expiresAt,
+  })
+
+  const resend = new Resend('re_Ch75DQsY_6ori4vYTyE2MUvWLprz2sQCD');
+
+  await resend.emails.send({
+    from: 'magi@varslinger.bondebridgeforbundet.no',
+    to: email,
+    subject: 'Pålogging til Bondebridgeforbundet',
+    html: `
+      <p>Hei ${user.full_name}</p>
+      <p>Her er din magiske lenke for å logge inn på Bondebridgeforbundet:</p>
+      <p><a href="https://bondebridgeforbundet.no/login/${randomString}">https://bondebridgeforbundet.no/login/${randomString}</a></p>
+      <p>Lenken er gyldig i 1 time.</p>
+      <p>Med vennlig hilsen</p>
+      <p>Bondebridgeforbundet</p>
+    `,
+  });
+
+  return {
+    success: true,
+    errors: {},
+  }
+}
