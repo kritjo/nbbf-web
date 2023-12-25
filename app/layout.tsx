5384d70416fc8a51c4066ef1dc2a157e881958dc
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-25 21:31:32 +0100

    tanstack query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import {ReactNode} from "react";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import {ThemeProvider} from "@/components/theme-provider";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -20,22 +21,26 @@ export default function RootLayout({
                                    }: {
   children: ReactNode
 }) {
+  const queryClient = new QueryClient()
+
   return (
     <html lang="en">
     <body className={inter.className}>
-    <section className="min-h-screen bg-gray-900 text-white">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        forcedTheme="dark"
-        enableSystem={false}
-        disableTransitionOnChange
-      >
-        <Navbar/>
-        {children}
-        <Footer/>
-      </ThemeProvider>
-    </section>
+    <QueryClientProvider client={queryClient}>
+      <section className="min-h-screen bg-gray-900 text-white">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          forcedTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <Navbar/>
+          {children}
+          <Footer/>
+        </ThemeProvider>
+      </section>
+    </QueryClientProvider>
     <Analytics/>
     <SpeedInsights/>
     </body>
