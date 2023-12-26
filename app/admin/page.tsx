d1cd89bb327b64bdbd90568b0aba1e5b3bd8a99b
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-26 13:50:22 +0100

    Admin boilerplate

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <main>
+      <section className="flex flex-col items-center justify-center py-12">
+        <p className="text-2xl font-bold">Du er nå innlogget. Type: Admin</p>
+      </section>
+    </main>
+  )
+}
