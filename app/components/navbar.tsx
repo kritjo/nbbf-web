a8f9531af3feab7a94a30f7eb9711118e25772aa
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-26 12:15:00 +0100

    navbar flex

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <Link href="#">
         <img className="w-24 h-24" src="/nbbf-nt-nbg.png" alt="Norges Bondebridgeforbund Logo"/>
       </Link>
-      <nav className="space-x-4">
+      <nav className="space-x-4 flex">
         <Link className="text-lg font-semibold hover:underline" href="/">
           Hjem
         </Link>
