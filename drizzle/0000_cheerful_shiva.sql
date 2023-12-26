ba373a2dc5466815dc5035819408f9beed7b5f87
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-26 15:06:34 +0100

    Reset drizzle

--- drizzle/0000_cheerful_shiva.sql
+++ drizzle/0000_cheerful_shiva.sql
@@ -1,6 +1,12 @@
+DO $$ BEGIN
+ CREATE TYPE "role_enum" AS ENUM('medlem', 'styre', 'admin');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "applications" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" integer,
+	"email" varchar(256) NOT NULL,
 	"title" varchar(256) NOT NULL,
 	"content" varchar(256) NOT NULL,
 	"approved" boolean NOT NULL,
@@ -9,22 +15,19 @@ CREATE TABLE IF NOT EXISTS "applications" (
 	"created_at" timestamp with time zone NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "roles" (
+CREATE TABLE IF NOT EXISTS "magic_links" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar(256) NOT NULL,
-	"description" varchar(256) NOT NULL,
-	CONSTRAINT "roles_name_unique" UNIQUE("name")
-);
---> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "user_roles" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" integer,
-	"role_id" integer
+	"user_id" integer NOT NULL,
+	"slug" varchar(64) NOT NULL,
+	"used" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL,
+	CONSTRAINT "magic_links_slug_unique" UNIQUE("slug")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user_sessions" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"user_id" integer,
+	"user_id" integer NOT NULL,
 	"token" varchar(256) NOT NULL,
 	"created_at" timestamp with time zone NOT NULL,
 	"expires_at" timestamp with time zone NOT NULL,
@@ -36,15 +39,10 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"email" varchar(256) NOT NULL,
 	"full_name" varchar(256) NOT NULL,
 	"created_at" timestamp with time zone NOT NULL,
+	"role" "role_enum" NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email"),
 	CONSTRAINT "users_full_name_unique" UNIQUE("full_name")
 );
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "applications" ADD CONSTRAINT "applications_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "applications" ADD CONSTRAINT "applications_approved_status_by_users_id_fk" FOREIGN KEY ("approved_status_by") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
@@ -53,13 +51,7 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "magic_links" ADD CONSTRAINT "magic_links_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- drizzle/0003_wooden_wonder_man.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "magic_links" ALTER COLUMN "user_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_roles" ALTER COLUMN "user_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_roles" ALTER COLUMN "role_id" SET NOT NULL;
