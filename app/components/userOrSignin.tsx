920090b75849714797b2d7b8360ea4986bf9f33f
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-26 12:15:11 +0100

    dropdown signout menu or signin dialog

--- app/components/userOrSignin.tsx
+++ app/components/userOrSignin.tsx
@@ -1,9 +1,28 @@
 import SigninDialog from "@/components/signin-dialog";
-import {Skeleton} from "@/components/ui/skeleton";
+import {getAuthenticatedUser} from "@/actions/getAuthenticatedUser";
+import { cookies } from 'next/headers';
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import LogoutMenuButton from "@/components/ui/logout-menu-button";
 
-const UserOrSignin = () => {
+
+const UserOrSignin = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token');
+  if (!token) return <SigninDialog/>;
+  const user = await getAuthenticatedUser(token.value);
+  if (!user) return <SigninDialog/>;
+
   return (
-    <SigninDialog/>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <LogoutMenuButton/>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
