a2e27000f0b55cb723603becf9d74f77940cfaf8
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-26 13:50:29 +0100

    Member welcome msg

---
+++ app/medlem/page.tsx
@@ -0,0 +1,9 @@
+export default function Medlem() {
+  return (
+    <main>
+      <section className="flex flex-col items-center justify-center py-12">
+        <p className="text-2xl font-bold">Du er nå innlogget. Type: Medlem</p>
+      </section>
+    </main>
+  )
+}
