8d4d85a8325a544935d5539fbc8671bafbb35eaa
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 00:34:57 +0100

    Members tab comp

--- app/styresider/medlemmer.tsx
+++ app/styresider/medlemmer.tsx
@@ -1,5 +1,34 @@
+import {getMembers} from "../../actions/getMembers";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import {use} from "react";
+import {getAuthenticatedUser} from "../../actions/getAuthenticatedUser";
+import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "../../components/ui/table";
+
 export default function Medlemmer() {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token');
+  if (!token) redirect('/')
+  const members = use(getMembers(token.value));
+
   return (
-    <p>Medlemmer!</p>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>E-post</TableHead>
+          <TableHead>Fullt navn</TableHead>
+          <TableHead>Rolle</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {members?.map(member => (
+          <TableRow key={member.id}>
+            <TableCell>{member.email}</TableCell>
+            <TableCell>{member.full_name}</TableCell>
+            <TableCell>{member.role}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
