109edda14b5ff512116680e63dc5a382af1adcd3
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 00:55:41 +0100

    await the authenticated user

--- actions/getMembers.ts
+++ actions/getMembers.ts
@@ -5,8 +5,8 @@ import {getAuthenticatedUser} from "./getAuthenticatedUser";
 import {db} from "../db/connection";
 
 export const getMembers = async (token: string): Promise<User[]> => {
-  const authenticatedUser = getAuthenticatedUser(token, 'styre');
-  if (!authenticatedUser) {
+  const authenticatedUser = await getAuthenticatedUser(token, 'styre');
+  if (authenticatedUser === null) {
     return [];
   }
 
