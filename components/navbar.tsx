3110021118b026d7806abc106025feaa8cbe8b9e
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 00:56:01 +0100

    Add role pages to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
 import UserOrSignin from "./userOrSignin";
 import Image from "next/image";
+import {cookies} from "next/headers";
+import {use} from "react";
+import {getAuthenticatedUser} from "../actions/getAuthenticatedUser";
+import {hasMinimumRole} from "../lib/utils";
 
 const Navbar = () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token');
+  let user = null;
+  if (token) user = use(getAuthenticatedUser(token.value, 'medlem'))
+
   return (
     <header className="flex justify-between items-center px-8 py-4">
       <Link href="/">
@@ -15,6 +24,16 @@ const Navbar = () => {
         <Link className="text-lg font-semibold hover:underline" href={"/kontakt"}>
           Kontakt
         </Link>
+        { user && hasMinimumRole(user.role, 'medlem') &&
+            <Link className="text-lg font-semibold hover:underline" href={"/medlem"}>
+                Medlemssider
+            </Link>
+        }
+        { user && hasMinimumRole(user.role, 'styre') &&
+            <Link className="text-lg font-semibold hover:underline" href={"/styresider"}>
+                Styresider
+            </Link>
+        }
         <UserOrSignin/>
       </nav>
     </header>
