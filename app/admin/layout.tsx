c07e9f9ab04acc02d9b09a8d5bd3707d59ec5f14
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 00:51:07 +0100

    Protect role pages using layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,15 @@
-import {ReactNode} from "react";
+import {ReactNode, use} from "react";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import {getAuthenticatedUser} from "../../actions/getAuthenticatedUser";
 
 export default function AdminLayout({children}: { children: ReactNode }) {
+  const cookieStore = cookies();
+  const token = cookieStore.get('token');
+  if (!token) redirect('/')
+  const user = use(getAuthenticatedUser(token.value, 'admin'))
+  if (!user) redirect('/')
+
   return (
     <>
       {children}
--- app/medlem/layout.tsx
+++ app/medlem/layout.tsx
@@ -1,13 +1,13 @@
 import {ReactNode, use} from "react";
 import {cookies} from "next/headers";
-import {getAuthenticatedUser} from "../../actions/getAuthenticatedUser";
 import {redirect} from "next/navigation";
+import {getAuthenticatedUser} from "../../actions/getAuthenticatedUser";
 
-export default function StyreLayout({children}: { children: ReactNode }) {
+export default function MedlemLayout({children}: { children: ReactNode }) {
   const cookieStore = cookies();
   const token = cookieStore.get('token');
   if (!token) redirect('/')
-  const user = use(getAuthenticatedUser(token.value, 'styre'))
+  const user = use(getAuthenticatedUser(token.value, 'medlem'))
   if (!user) redirect('/')
 
   return (
