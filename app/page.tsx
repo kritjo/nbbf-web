a08207d4e7529ad33b162416788b7a2a436b270f
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 01:59:23 +0100

    Application Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import '@fontsource/pt-mono';
+import ApplicationForm from "../components/applicationForm";
 export default function Home() {
   return (
     <main>
@@ -10,38 +11,8 @@ export default function Home() {
       >
         <h1 className="text-xl font-bold mb-4 nbbf-font md:text-2xl xl:text-5xl">Norges Bondebridgeforbund</h1>
       </section>
-      <section className="flex flex-col items-center justify-center py-12" data-tooltip-target="tooltip-register">
-        <form className="flex flex-col space-y-4 p-6 mt-4 bg-white rounded-lg shadow-md opacity-20">
-          <div className="space-y-2">
-            <label className="text-sm font-medium text-gray-700" htmlFor="name">
-              Navn
-            </label>
-            <input
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              id="name"
-              placeholder="Ditt navn"
-              required
-              disabled
-            />
-          </div>
-          <div className="space-y-2">
-            <label className="text-sm font-medium text-gray-700" htmlFor="email">
-              E-post
-            </label>
-            <input
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
-              id="email"
-              placeholder="Din e-post"
-              required
-              type="email"
-              disabled
-            />
-
-          </div>
-          <button className="w-full px-3 py-2 text-white bg-blue-500 rounded-md" type="submit" disabled>
-            Send inn
-          </button>
-        </form>
+      <section className="flex flex-col items-center justify-center py-12">
+        <ApplicationForm/>
       </section>
       <section className="w-full py-12 bg-transparent text-white">
         <div className="container px-4 md:px-6">
---
+++ components/applicationForm.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger} from "./ui/dialog";
+import {Button} from "./ui/button";
+import {Label} from "./ui/label";
+import {Input} from "./ui/input";
+import {Textarea} from "./ui/textarea";
+import FormSubmitButton from "./ui/form-submit-button";
+import {useFormState} from "react-dom";
+import {sendMemberApplication} from "../actions/sendMemberApplication";
+
+export default function ApplicationForm() {
+  const [formState, formAction] = useFormState(sendMemberApplication, null);
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="default" size="lg">Send inn søknad om medlemsskap</Button>
+      </DialogTrigger>
+      <DialogContent className="w-full lg:max-w-3xl">
+        <DialogHeader>
+          <DialogTitle>Søknad om medlemsskap i <p className="nbbf-font inline">Norges Bondebridgeforbund</p></DialogTitle>
+        </DialogHeader>
+        {formState?.success ? (
+          <DialogDescription>
+            Din søknad er sendt inn, og vil bli behandlet så snart som mulig.
+          </DialogDescription>
+        ) : (
+          <form action={formAction}>
+            <Label htmlFor="full_name">Fullt navn</Label>
+            <Input type="text" name={"full_name"} required className="mb-1"/>
+            {formState?.errors?.full_name && (
+              <div id="name-error" style={{color: `#dc2626`}}>
+                {formState.errors.full_name.join(',')}
+              </div>
+            )}
+            <Label htmlFor="full_name">E-post addresse</Label>
+            <Input type="email" placeholder="" name={"email"} required className="mb-1"/>
+            {formState?.errors?.email && (
+              <div id="name-error" style={{color: `#dc2626`}}>
+                {formState.errors.email.join(',')}
+              </div>
+            )}
+            <Label htmlFor="title">Søknadens tittel</Label>
+            <Input type="text" placeholder="" name={"title"} required className="mb-1"/>
+            {formState?.errors?.title && (
+              <div id="name-error" style={{color: `#dc2626`}}>
+                {formState.errors.title.join(',')}
+              </div>
+            )}
+            <Label htmlFor="body">Søknadens innhold</Label>
+            <Textarea placeholder="Fortell litt om deg selv" name={"content"} required className="mb-1" />
+            <FormSubmitButton>
+              Send inn
+            </FormSubmitButton>
+          </form>
+        )}
+      </DialogContent>
+    </Dialog>
+  )
+}
