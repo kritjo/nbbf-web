66bfb820cca263f380202bdcfb693e22d24650f6
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 15:29:50 +0100

    Query prefetching and hydration boundary for members

--- app/styresider/medlemmer.tsx
+++ app/styresider/medlemmer.tsx
@@ -1,34 +1,23 @@
 import {getMembers} from "../../actions/getMembers";
 import {cookies} from "next/headers";
 import {redirect} from "next/navigation";
-import {use} from "react";
-import {getAuthenticatedUser} from "../../actions/getAuthenticatedUser";
-import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "../../components/ui/table";
+import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
+import MemberTable from "../../components/member-table";
 
-export default function Medlemmer() {
+export default async function Medlemmer() {
   const cookieStore = cookies();
   const token = cookieStore.get('token');
   if (!token) redirect('/')
-  const members = use(getMembers(token.value));
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['members'],
+    queryFn: () => getMembers(token.value),
+  });
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>E-post</TableHead>
-          <TableHead>Fullt navn</TableHead>
-          <TableHead>Rolle</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {members?.map(member => (
-          <TableRow key={member.id}>
-            <TableCell>{member.email}</TableCell>
-            <TableCell>{member.full_name}</TableCell>
-            <TableCell>{member.role}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <MemberTable token={token}/>
+    </HydrationBoundary>
   )
 }
---
+++ components/member-table.tsx
@@ -0,0 +1,31 @@
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "./ui/table";
+import {useQuery} from "@tanstack/react-query";
+import {RequestCookie} from "next/dist/compiled/@edge-runtime/cookies";
+import {getMembers} from "../actions/getMembers";
+
+const MemberTable = ({token}: {token: RequestCookie}) => {
+  const { data } = useQuery({ queryKey: ['members'], queryFn: () => getMembers(token.value) })
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>E-post</TableHead>
+          <TableHead>Fullt navn</TableHead>
+          <TableHead>Rolle</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data?.map(member => (
+          <TableRow key={member.id}>
+            <TableCell>{member.email}</TableCell>
+            <TableCell>{member.full_name}</TableCell>
+            <TableCell>{member.role}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+export default MemberTable
