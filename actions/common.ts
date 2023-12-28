74b1e727fe05a815c2e9f6ed0c804b68b48dff08
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 14:35:44 +0100

    Some types for applcation stuffs

--- actions/common.ts
+++ actions/common.ts
@@ -1,6 +1,18 @@
+import {Application, User} from "../db/schema";
+
 export type FormResponse = {
   success: boolean,
   errors: {
     [key: string]: string[]
   }
+}
+
+export type GetApplicationsResponse = {
+  applications: Application
+  users: User | null
+}
+
+export enum ApplicationAction {
+  'approved' = 'approved',
+  'rejected' = 'rejected'
 }
