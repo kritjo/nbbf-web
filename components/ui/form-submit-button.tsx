072867d6fb89bbfe30977793952c38c33ecb8a63
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 12:55:37 +0100

    Loader spinner on pending submitt

--- components/ui/form-submit-button.tsx
+++ components/ui/form-submit-button.tsx
@@ -1,5 +1,6 @@
 import {Button} from "./button";
 import {useFormStatus} from "react-dom";
+import {Loader2} from "lucide-react";
 
 // @ts-ignore // TODO: Fix this
 const FormSubmitButton = ({children, ...props }) => {
@@ -7,6 +8,9 @@ const FormSubmitButton = ({children, ...props }) => {
   return (
 
     <Button className="mt-4" variant="outline" size="sm" type="submit" disabled={pending} aria-busy={pending} aria-disabled={pending} {...props}>
+      {pending && (
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      )}
       {children}
     </Button>
   );
