931eaf1ccac6a425f158acb1c3fa8e3ea0603b82
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 14:35:17 +0100

    Application action server function

---
+++ actions/applicationAction.ts
@@ -0,0 +1,76 @@
+'use server'
+
+import {getAuthenticatedUser} from "./getAuthenticatedUser";
+import {ApplicationAction, FormResponse} from "./common";
+import {applications} from "../db/schema";
+import {db} from "../db/connection";
+import {eq} from "drizzle-orm";
+import {Resend} from "resend";
+
+export const applcationAction = async (token: string, applicationId: number, action: ApplicationAction): Promise<FormResponse> => {
+  const authenticatedUser = await getAuthenticatedUser(token, 'styre');
+  if (authenticatedUser === null) {
+    return {
+      success: false,
+      errors: {
+        token: ['Invalid token']
+      }
+    }
+  }
+
+  const application = await db.query.applications.findFirst({
+    where: eq(applications.id, applicationId),
+  });
+
+  if (!application) {
+    return {
+      success: false,
+      errors: {
+        application: ['Invalid application id']
+      }
+    }
+  }
+
+  await db.update(applications).set({
+    status: action,
+    status_by: authenticatedUser.id,
+    status_at: new Date(),
+  }).where(eq(applications.id, applicationId));
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  if (action === ApplicationAction.approved) {
+    console.log(`Application ${applicationId} approved by ${authenticatedUser.email}`);
+    await resend.emails.send({
+      from: 'soknader@varslinger.bondebridgeforbundet.no',
+      to: application.email,
+      subject: 'Velkommen til Bondebridgeforbundet',
+      html: `
+      <p>Hei ${application.full_name}</p>
+      <p>Din søknad om medlemskap i Bondebridgeforbundet er godkjent.</p>
+      <p>Vi gleder oss til å se deg på neste turnering!</p>
+      <p>Med vennlig hilsen</p>
+      <p>Bondebridgeforbundet</p>
+    `,
+    });
+  } else if (action === ApplicationAction.rejected) {
+    console.log(`Application ${applicationId} rejected by ${authenticatedUser.email}`);
+    await resend.emails.send({
+      from: 'soknader@varslinger.bondebridgeforbundet.no',
+      to: application.email,
+      subject: 'Velkommen til Bondebridgeforbundet',
+      html: `
+      <p>Hei ${application.full_name}</p>
+      <p>Din søknad om medlemskap i Bondebridgeforbundet er avslått.</p>
+      <p>Vi håper du vil prøve igjen senere.</p>
+      <p>Med vennlig hilsen</p>
+      <p>Bondebridgeforbundet</p>
+    `,
+    });
+  }
+
+  return {
+    success: true,
+    errors: {}
+  }
+}
