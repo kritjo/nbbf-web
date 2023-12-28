52eed42ae90be2d502a7a0dbd1ac024bddc7e683
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-28 01:34:33 +0100

    sendMemberApplication boiler

---
+++ actions/sendMemberApplication.ts
@@ -0,0 +1,56 @@
+'use server'
+
+import {FormResponse} from "./common";
+import {z} from "zod";
+import {db} from "../db/connection";
+import {applications} from "../db/schema";
+
+const schema = z.object({
+  full_name: z.string({
+    invalid_type_error: 'Invalid Full Name',
+  }),
+  email: z.string({
+    invalid_type_error: 'Invalid Email',
+  }),
+  title: z.string({
+    invalid_type_error: 'Invalid Title',
+  }),
+  content: z.string({
+    invalid_type_error: 'Invalid Content',
+  }),
+})
+
+export const sendMemberApplication = async (_: any, formData: FormData): Promise<FormResponse> => {
+const validatedFields = schema.safeParse({
+    full_name: formData.get('full_name'),
+    email: formData.get('email'),
+    title: formData.get('title'),
+    content: formData.get('content'),
+  });
+
+  // Return early if the form data is invalid
+  if (!validatedFields.success) {
+    return {
+      success: false,
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+
+  console.log(`Member application for ${validatedFields.data.email} received`);
+
+  await db.insert(applications).values({
+    full_name: validatedFields.data.full_name,
+    email: validatedFields.data.email,
+    title: validatedFields.data.title,
+    content: validatedFields.data.content,
+    approved: false,
+    approved_status_by: null,
+    approved_status_at: null,
+    created_at: new Date(),
+  });
+
+  return {
+    success: true,
+    errors: {}
+  }
+}
