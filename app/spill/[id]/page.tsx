ffeb6d57b81c3beb4e5bc378dd49392cc41ec507
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-29 15:54:28 +0100

    delete function working

--- app/spill/[id]/page.tsx
+++ app/spill/[id]/page.tsx
@@ -81,10 +81,10 @@ export default function GameInstance({ params }: { params: { id: string } }) {
               </TableHeader>
               <TableBody>
                 {membersInGame?.map((member) => (
-                  <GameMemberRow gamePlayerId={member.id} name={member.full_name} key={member.id}/>
+                  <GameMemberRow gamePlayerId={member.id} name={member.full_name} key={member.id} tokenValue={token.value}/>
                 ))}
                 {guestsInGame?.map((guest) => (
-                  <GameMemberRow gamePlayerId={guest.gamePlayersId} name={guest.guest_name} key={guest.gamePlayersId}/>
+                  <GameMemberRow gamePlayerId={guest.gamePlayersId} name={guest.guest_name} key={guest.gamePlayersId} tokenValue={token.value}/>
                 ))}
               </TableBody>
             </Table>
--- components/game-member-row.tsx
+++ components/game-member-row.tsx
@@ -1,13 +1,28 @@
+'use client'
+
 import {TableCell, TableRow} from "./ui/table";
 import {Button} from "./ui/button";
+import {useTransition} from "react";
+import {deleteGameUser} from "../actions/deleteGameUser";
+import {Loader2} from "lucide-react";
 
-const GameMemberRow = ({gamePlayerId, name}: {gamePlayerId: number, name: string}) => {
+const GameMemberRow = ({gamePlayerId, name, tokenValue}: {gamePlayerId: number, name: string, tokenValue: string}) => {
+  const [isPending, startTransition] = useTransition();
+
+  const removeMember = async () => {
+    startTransition(async () => {
+      const success = await deleteGameUser(tokenValue, gamePlayerId);
+      if (!success) alert('Noe gikk galt');
+    })
+  }
+
   return (
     <TableRow key={gamePlayerId}>
       <TableCell>{name}</TableCell>
       <TableCell>Medlem</TableCell>
       <TableCell>
-        <Button variant="outline" className="text-red-500">
+        <Button variant="outline" className="text-red-500" onClick={() => removeMember()} disabled={isPending}>
+          {isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
           Fjern
         </Button>
       </TableCell>
