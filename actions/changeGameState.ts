373e9fd2169e198bca12431df005e1df96865e6f
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-29 19:48:43 +0100

    changeGameState server action

---
+++ actions/changeGameState.ts
@@ -0,0 +1,35 @@
+'use server'
+
+import {getAuthenticatedUser} from "./getAuthenticatedUser";
+import {games} from "../db/schema";
+import {eq} from "drizzle-orm";
+import {db} from "../db/connection";
+import {revalidatePath} from "next/cache";
+
+export const changeGameState = async (token: string, gameID: number, state: 'started' | 'finished'): Promise<boolean> => {
+  const authenticatedUser = await getAuthenticatedUser(token, 'medlem');
+  if (authenticatedUser === null) {
+    return false;
+  }
+
+  const game = await db.query.games.findFirst({
+    where: eq(games.id, gameID),
+  });
+
+  if (game === undefined) {
+    return false;
+  }
+
+  if (game.created_by !== authenticatedUser.id && authenticatedUser.role !== 'admin') {
+    return false;
+  }
+
+  await db.update(games).set({
+    status: state,
+  }).where(eq(games.id, gameID));
+
+  revalidatePath('/spill')
+  revalidatePath(`/spill/${gameID}`)
+
+  return true;
+}
