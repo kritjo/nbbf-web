2d4e70b626e8898cbc431bf1044a41ef4a3cab23
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-29 12:45:31 +0100

    Revalidate /spill on delete

---
+++ actions/deleteGame.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import {getAuthenticatedUser} from "./getAuthenticatedUser";
+import {db} from "../db/connection";
+import {games} from "../db/schema";
+import {eq} from "drizzle-orm";
+import {revalidatePath} from "next/cache";
+
+export const deleteGame = async (token: string, gameID: number): Promise<boolean> => {
+  const authenticatedUser = await getAuthenticatedUser(token, 'medlem');
+  if (authenticatedUser === null) {
+    return false;
+  }
+
+  const game = await db.query.games.findFirst({
+    where: eq(games.id, gameID),
+  });
+
+  if (game === undefined) {
+    return false;
+  }
+
+  if (game.created_by !== authenticatedUser.id && authenticatedUser.role !== 'admin') {
+    return false;
+  }
+
+  await db.delete(games).where(eq(games.id, gameID));
+
+  revalidatePath('/spill');
+
+  return true;
+}
