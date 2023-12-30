5ec70c36ea91fa76995ad797d02dd92e75bc735c
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-30 19:49:01 +0100

    Prefetch queries and hydrate

--- app/spill/[id]/page.tsx
+++ app/spill/[id]/page.tsx
@@ -1,10 +1,34 @@
-import GameViewServer from "../../../components/game-view-server";
+import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
+import GameViewClient from "../../../components/game-view-client";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import {getGame} from "../../../actions/getGame";
+import {getPlayersInGame} from "../../../actions/getPlayersInGame";
+import {getMembersNotInGame} from "../../../actions/getMembersNotInGame";
 
 export default function GameInstance({ params }: { params: { id: string } }) {
   const { id } = params;
-  const game_id = parseInt(id);
+  const gameId = parseInt(id);
+  const token = cookies().get('token');
+  if (!token) redirect('/');
+
+  const queryClient = new QueryClient();
+  queryClient.prefetchQuery({
+    queryKey: ['game', gameId],
+    queryFn: () => getGame(token.value, gameId)
+  })
+  queryClient.prefetchQuery({
+    queryKey: ['playersInGame', gameId],
+    queryFn: () => getPlayersInGame(token.value, gameId)
+  })
+  queryClient.prefetchQuery({
+    queryKey: ['membersNotInGame', gameId],
+    queryFn: () => getMembersNotInGame(token.value, gameId),
+  });
 
   return (
-    <GameViewServer gameId={game_id}/>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <GameViewClient gameId={gameId} tokenValue={token.value}/>
+    </HydrationBoundary>
   )
 }
