33a9501ffe51fa1cec6354489dc65a684e131e76
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-30 12:58:10 +0100

    setBidsTricks server function

---
+++ actions/setBidsTricks.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import {getAuthenticatedUser} from "./getAuthenticatedUser";
+import {db} from "../db/connection";
+import {eq} from "drizzle-orm";
+import {gameRoundPlayers, gameRounds, games} from "../db/schema";
+
+export const setBidsTricks = async (token: string, roundGamePlayer: number, bids: number, tricks: number): Promise<boolean> => {
+  const authenticatedUser = await getAuthenticatedUser(token, 'medlem');
+  if (authenticatedUser === null) {
+    return false;
+  }
+
+  const roundGamePlayerQuery = await db.query.gameRoundPlayers.findFirst({
+    where: eq(gameRoundPlayers.id, roundGamePlayer),
+  });
+
+  if (roundGamePlayerQuery === undefined) {
+    return false;
+  }
+
+  const gameRound = await db.query.gameRounds.findFirst({
+    where: eq(gameRounds.id, roundGamePlayerQuery.game_round),
+  });
+
+  if (gameRound === undefined) {
+    return false;
+  }
+
+  const game = await db.query.games.findFirst({
+    where: eq(games.id, gameRound.game),
+  });
+
+  if (game === undefined) {
+    return false;
+  }
+
+  if (game.created_by !== authenticatedUser.id && authenticatedUser.role !== 'admin') {
+    return false;
+  }
+
+  await db.update(gameRoundPlayers).set({
+    bid: bids,
+    tricks: tricks,
+  }).where(eq(gameRoundPlayers.id, roundGamePlayer));
+
+  return true;
+}
