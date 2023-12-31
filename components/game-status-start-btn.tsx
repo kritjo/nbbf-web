5281571084e0b044f8b9ed3ac98242d636a9b012
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-31 13:26:30 +0100

    Game start using mutations

--- components/game-status-start-btn.tsx
+++ components/game-status-start-btn.tsx
@@ -1,24 +1,48 @@
 'use client'
 
 import {Button} from "./ui/button";
-import {useTransition} from "react";
+import {useState} from "react";
 import {changeGameState} from "../actions/changeGameState";
 import {Loader2} from "lucide-react";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {newRound} from "../actions/newRound";
 
 const GameStatusStartBtn = ({tokenValue, gameId}: {tokenValue: string, gameId: number}) => {
-  const [isPending, startTransition] = useTransition();
-  const handleStart = async () => {
-    startTransition(async () => {
-      const resp = await changeGameState(tokenValue, gameId, 'started');
-      if (resp) await newRound(tokenValue, gameId);
-    });
-  }
+  const [isPending, setIsPending] = useState(false);
+  const queryClient = useQueryClient();
+
+  const handleNewRound = useMutation({
+    mutationFn: () => newRound(tokenValue, gameId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['game', gameId]});
+    },
+    onError: (err, newGuest, context) => {
+      console.log(err);
+    },
+    onSettled: () => {
+      setIsPending(false);
+    }
+  })
+
+  const handleStart = useMutation({
+    mutationFn: () => changeGameState(tokenValue, gameId, 'started'),
+    onMutate: async () => {
+      setIsPending(true);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['game', gameId]});
+      handleNewRound.mutate();
+    },
+    onError: (err, newGuest, context) => {
+      console.log(err);
+      setIsPending(false);
+    },
+  })
 
   return (
     <Button
       className="text-white bg-green-500"
-      onClick={() => handleStart()}
+      onClick={() => handleStart.mutate()}
       disabled={isPending}
     >
       {isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
