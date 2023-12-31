e7d18d7d8e90597edb54be4b990e96b7c4e30155
Kristian Tjelta Johansen <kritjo@kritjo.com>
2023-12-31 12:52:52 +0100

    Add guest box uses mutation from tanstack query instead

--- components/add-guest-box.tsx
+++ components/add-guest-box.tsx
@@ -6,18 +6,51 @@ import {Input} from "./ui/input";
 import {useState, useTransition} from "react";
 import {Loader2} from "lucide-react";
 import {addMemberToGame} from "../actions/addMemberToGame";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {PlayersInGameResponse} from "../actions/common";
 
 const AddGuestBox = ({gameId, tokenValue}: {gameId: number, tokenValue: string}) => {
   const [open, setOpen] = useState(false);
   const [guestName, setGuestName] = useState('');
-  const [isPending, startTransition] = useTransition();
+  const [isPending, setIsPending] = useState(false);
+  const queryClient = useQueryClient();
 
-  const handleAddGuest = async () => {
-    startTransition(async () => {
-      await addMemberToGame(tokenValue, gameId, -1, guestName);
+  const handleAddGuest = useMutation({
+    mutationFn: (guestName: string) => addMemberToGame(tokenValue, gameId, -1, guestName),
+    onMutate: async (newGuest) => {
+      setIsPending(true);
+      await queryClient.cancelQueries({queryKey: ['playersInGame', gameId]});
+      const previousPlayersInGame= queryClient.getQueryData<PlayersInGameResponse>(['playersInGame', gameId]);
+      if (!previousPlayersInGame) throw new Error('Missing previousPlayersInGame, while handling onMutate in handleAddGuest mutation');
+      queryClient.setQueryData<PlayersInGameResponse>(['playersInGame', gameId], (old) => {
+        if (!old) throw new Error('Missing old, while handling onMutate in handleAddGuest mutation');
+        return {
+          ...old,
+          players: [...old.players, {
+            id: -1,
+            name: newGuest,
+            type: 'gjest',
+            userId: null,
+          }]
+        }
+      });
+      return { previousPlayersInGame };
+    },
+    onError: (err, newGuest, context) => {
+      console.log(err);
+      if (context?.previousPlayersInGame === undefined) {
+        throw new Error('Missing context.previousMembers, while handling error in handleAddGuest mutation')
+      }
+      queryClient.setQueryData(['membersNotInGame', gameId], context.previousPlayersInGame);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['membersNotInGame', gameId]});
       setOpen(false);
-    });
-  };
+    },
+    onSettled: () => {
+      setIsPending(false);
+    }
+  })
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -31,7 +64,7 @@ const AddGuestBox = ({gameId, tokenValue}: {gameId: number, tokenValue: string})
         <Input placeholder="Navn på gjest" value={guestName} onChange={(e) => setGuestName(e.target.value)} />
         <Button
           className="mt-4"
-          onClick={handleAddGuest}
+          onClick={() => handleAddGuest.mutate(guestName)}
           disabled={isPending}
         >
           {isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
