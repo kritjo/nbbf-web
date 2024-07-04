d86d12357a99b10bb7102189c8663548df3f1c58
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-07-04 18:55:53 +0200

    Redbull ad

--- app/page.tsx
+++ app/page.tsx
@@ -14,15 +14,20 @@ export default function Home() {
       <section className="flex flex-col items-center justify-center py-12">
         <ApplicationForm/>
       </section>
-      <section className="w-full py-12 bg-transparent text-white">
+      <section className="w-full space-y-12 bg-transparent text-white">
         <div className="container px-4 md:px-6">
-          <div className="flex flex-col items-center space-y-4 text-center">
+          <div className="flex flex-col items-center space-y-12 text-center">
             <div className="space-y-2">
               <p className="mx-auto max-w-[700px] text-lg md:text-xl dark:text-zinc-400">
                 Sjakk er ikke det eneste spillet der konger og dronninger kan demonstrere sin taktiske overlegenhet -
                 Forbundet tilbyr en unik utfordring der hver bonde kan bli en konge på slagmarken.
               </p>
               <span className="text-sm italic">- Styrets Leder</span>
+            </div>
+            <div className="w-full max-w-md rounded-lg bg-accent p-6 shadow-lg">
+              <p className="text-accent-foreground">Vi retter en takk til Red Bull som holder oss våkne og skarpe
+                helt til
+                siste stikk.</p>
             </div>
           </div>
         </div>
